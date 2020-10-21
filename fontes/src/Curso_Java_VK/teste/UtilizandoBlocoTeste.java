48645f4778514358d7a597965f075a37025dc20a
Vinicius Kammer <69961@proway.treina>
2020-10-21 19:13:11 -0300

    Novos Codigos Loop e Vetores

---
+++ fontes/src/Curso_Java_VK/teste/UtilizandoBlocoTeste.java
@@ -0,0 +1,24 @@
+package Curso_Java_VK.teste;
+
+import java.util.Scanner;
+
+public class UtilizandoBlocoTeste {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try {//tente executar
+			System.out.println("Escreva sua idade:");
+			int idade = Integer.parseInt(sc.nextLine());
+			//...
+			System.out.println("Sem erro");
+			System.out.println(idade);
+		} catch (Exception e) {
+			//se der qualquer erro executa esse bloco
+			e.printStackTrace();//escreve detalhes do erro
+		} finally {
+			//independente se der erro ou não executa esse bloco
+			sc.close();
+		}		
+	}
+
+}
